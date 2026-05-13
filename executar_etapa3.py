809633c1e5a10fae087ce188266792d2c747083e
RodriBatalha <Rodrigobatalha001@gmail.com>
2026-05-12 21:34:46 -0300

    Delete executar_etapa3.py

--- executar_etapa3.py
+++
@@ -1,2 +0,0 @@
-"""Versão resumida executável da Etapa 3. Para a entrega, use o notebook .ipynb."""
-print("Abra o notebook AgTech_Etapa3_RNA_SistemaEspecialista_Gemini.ipynb no Google Colab para executar a versão completa.")
